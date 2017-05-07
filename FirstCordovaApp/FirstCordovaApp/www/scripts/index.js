e4a949490f803d0d22e9a3ed8b3f0d804505e4a3
nikolcar <mitic.nikolca94@gmail.com>
2017-05-07 19:11:34 +0200

    SQLite

--- FirstCordovaApp/FirstCordovaApp/www/scripts/index.js
+++ FirstCordovaApp/FirstCordovaApp/www/scripts/index.js
@@ -6,18 +6,14 @@
     "use strict";
 
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
-
+    
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        //var parentElement = document.getElementById('deviceready');
-        //var listeningElement = parentElement.querySelector('.listening');
-        //var receivedElement = parentElement.querySelector('.received');
-        //listeningElement.setAttribute('style', 'display:none;');
-        //receivedElement.setAttribute('style', 'display:block;');
+        
         var cam = document.getElementById('camera');
         
         if (cam) {
@@ -28,8 +24,7 @@
                 });
             });
         }
-
-        
+       
     };
 
     function onPause() {
@@ -100,7 +95,6 @@
             ctx.textAlign = "center";
             ctx.strokeText("Ph", 113, 100);
         }
-
     }
 
 
@@ -121,5 +115,12 @@
     function onFail(message) {
         alert('Failed because: ' + message);
     }
+
+
+    
+
+    
+
+    
 
 })();
