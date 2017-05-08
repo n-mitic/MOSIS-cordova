cd432c88aca0e3ed1e2577d185874a4368b5b144
nikolcar <mitic.nikolca94@gmail.com>
2017-05-08 06:23:26 +0200

    camera, accelerometar, intervals

--- FirstCordovaApp/FirstCordovaApp/www/scripts/showmap.js
+++ FirstCordovaApp/FirstCordovaApp/www/scripts/showmap.js
@@ -17,10 +17,13 @@
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
         //app.receivedEvent('deviceready');
-        app.runGeolocation();
+        
+        app.runMaps();
     },
 
-    runGeolocation: function () {
+
+
+    runMaps: function () {
         var options = {
             maximumAge: 3000,
             timeout: 5000,
@@ -32,17 +35,34 @@
         var Longitude = localStorage.Longitude;
         var updatedLatitude;
         var updatedLongitude;
-        var interval;
+        var interval = false;
+        var x1 = 0, x2 = 0, y1 = 0, y2 = 0, z1 = 0, z2 = 0;
+
+        function onSucess(acceleration) { x1 = acceleration.x;  y1 = acceleration.y;    z1 = acceleration.z; }
+
+        function onError(e) { alert('onError!'); }
+
+        setInterval(function () {
+            var change = Math.abs(x1 - x2 + y1 - y2 + z1 - z2);
+
+            if (change > 25) {
+                if(interval) { stopInterval(); alert('stopped!'); }
+                else { startInterval(); alert('started!'); }
+            }
+            x2 = x1;    y2 = y1;    z2 = z1;
+        }, 100);
+
+        var watchID = navigator.accelerometer.watchAcceleration(onSucess, onError, { frequency: 100 });
 
         var onMapSuccess = function (position) {
             if (Type == 0) {
-                startInterval();
+                //startInterval();
                 Latitude = position.coords.latitude;
                 Longitude = position.coords.longitude;
             }
             else {
                 Type = 0;
-                stopInterval();
+                //stopInterval();
             }
             getMap(Latitude, Longitude, "map");
         }
@@ -65,6 +85,25 @@
             map.setCenter(marker.getPosition());
         }
 
+        function querySuccess(tx, results) {
+            var len = results.rows.length;
+            var mini = 0;
+            var minlen = Number.MAX_SAFE_INTEGER;
+            for (var i = 0; i < len; i++) {
+                var current = Math.sqrt(Math.pow(results.rows.item(i).lati - updatedLatitude, 2)
+                            + Math.pow(results.rows.item(i).lon - updatedLongitude, 2));
+                if (current < minlen) {
+                    minlen = current;
+                    mini = i;
+                }
+            }
+            alert('Closest parking is ' + results.rows.item(mini).name)
+        }
+
+        function queryDB(tx) {
+            tx.executeSql('SELECT * FROM Spots', [], querySuccess, errorCB);
+        }
+
         var onMapWatchSuccess = function (position) {
             updatedLatitude = position.coords.latitude;
             updatedLongitude = position.coords.longitude;
@@ -72,6 +111,7 @@
             if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
                db = window.openDatabase("Parking", "1.0", "Cordova", 200000);
                db.transaction(queryDB, errorCB);
+               Latitude = updatedLatitude; Longitude = updatedLongitude;
             }
         }
 
@@ -87,12 +127,13 @@
         function startInterval() {
             interval = window.setInterval(function () {
                 watchMapPosition();
-                alert('triger');
+                alert('trigger');
             }, 10000)
         }
 
         function stopInterval() {
             clearInterval(interval);
+            interval = false;
         }
 
         function populateDB(tx) {
@@ -103,25 +144,7 @@
         function errorCB(err) {
             alert("Error processing SQL: " + err.code);
         }
-
-        function queryDB(tx) {
-            tx.executeSql('SELECT * FROM Spots', [], querySuccess, errorCB);
-        }
-
-        function querySuccess(tx, results) {
-            var len = results.rows.length;
-            var mini = 0;
-            var minlen = Number.MAX_SAFE_INTEGER;
-            for (var i = 0; i < len; i++) {
-                var current = Math.sqrt(Math.pow(results.rows.item(i).lati - updatedLatitude, 2)
-                            + Math.pow(results.rows.item(i).lon - updatedLongitude, 2));
-                if (current < minlen) {
-                    minlen = current;
-                    mini = i;
-                }
-            }
-            alert('Closest parking is '+results.rows.item(mini).name)
-        }
+               
 
         var db = window.openDatabase("Parking", "1.0", "Cordova", 200000);
         db.transaction(populateDB, errorCB);
